a2e511314534773ea202a6acc75aa5b64b39b9c9
Da.Liu <liuda69@qq.com>
2018-05-16 17:44:53 +0800

    update

--- MQ_TEST/src/Receive.java
+++ MQ_TEST/src/Receive.java
@@ -1,81 +1,82 @@
 
-import javax.jms.Connection;  
-import javax.jms.ConnectionFactory;  
-import javax.jms.Destination;  
-import javax.jms.JMSException;  
-import javax.jms.Message;  
-import javax.jms.MessageConsumer;  
-import javax.jms.Session;  
-import javax.jms.TextMessage;  
-   
-import org.apache.activemq.ActiveMQConnectionFactory;  
-   
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
-public class Receive {  
-	     private static final String url = "failover:(tcp://192.168.13.253:61616,tcp://192.168.11.68:61616)?jms.useAsyncSend=true&randomize=false";//"tcp://192.168.11.105:61616";//
-         private static final String QUEUE_NAME = "G2Queue";  
-   
-         public void receiveMessage() throws JMSException {  
-                   Connection connection = null;  
-                   try{  
-                            try{  
-                                     ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);  
-                                     connection= connectionFactory.createConnection();  
-                            }catch (Exception e) {  
-                                     System.out.println(e.toString());  
-                            }  
-                            connection.start();  
-                            Session session = connection.createSession(false,  Session.AUTO_ACKNOWLEDGE);  
-                            Destination destination = session.createQueue(QUEUE_NAME);  
-                            
-                            MessageConsumer consumer = session.createConsumer(destination);  
-                             
-                            consumeMessagesAndClose(connection,session, consumer);  
-                   }catch (Exception e) {  
-                            System.out.println(e.toString());  
-                   } finally {
-                	   if(connection!=null)
-                		   connection.close();
-                	   
-                   } 
-         }  
-         
-         protected void consumeMessagesAndClose(Connection connection,  
-                            Session session, MessageConsumer consumer) throws JMSException {  
-                   do{  
-                            Message message = consumer.receive(10);  
-                            if("close".equals(message)){  
-                                     consumer.close();  
-                                     session.close();  
-                                     connection.close();  
-                            }  
-                            if(message != null) {  
-                                     onMessage(message);  
-                            }  
-                   }while (true);  
-                    
-         }  
-   
-         public void onMessage(Message message) {  
-                   try{  
-                            if(message instanceof TextMessage) {  
-                                     TextMessage txtMsg = (TextMessage) message;  
-                                     String msg = txtMsg.getText();  
-                                     System.out.println("Received:" + msg);  
-                            }  
-                   }catch (Exception e) {  
-                            e.printStackTrace();  
-                   }  
-   
-         }  
-   
-         public static void main(String args[]) {  
-                   Receive rm = new Receive();  
-                   try {
-					rm.receiveMessage();
-				} catch (JMSException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}  
-         }  
-}  
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Receive {
+	 private static final String url = "failover:(tcp://192.168.13.253:61616,tcp://192.168.11.68:61616)?jms.useAsyncSend=true&randomize=false";//"tcp://192.168.11.105:61616";//
+	//private static final String url = "failover:(ws://192.168.13.253:61614/stomp,ws://192.168.11.68:61614/stomp";
+
+	private static final String QUEUE_NAME = "G2Queue";
+
+	public void receiveMessage() throws JMSException {
+		Connection connection = null;
+		try {
+			try {
+				ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+				connection = connectionFactory.createConnection();
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = session.createQueue(QUEUE_NAME);
+
+			MessageConsumer consumer = session.createConsumer(destination);
+
+			consumeMessagesAndClose(connection, session, consumer);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			if (connection != null)
+				connection.close();
+
+		}
+	}
+
+	protected void consumeMessagesAndClose(Connection connection, Session session, MessageConsumer consumer)
+			throws JMSException {
+		do {
+			Message message = consumer.receive(10);
+			if ("close".equals(message)) {
+				consumer.close();
+				session.close();
+				connection.close();
+			}
+			if (message != null) {
+				onMessage(message);
+			}
+		} while (true);
+
+	}
+
+	public void onMessage(Message message) {
+		try {
+			if (message instanceof TextMessage) {
+				TextMessage txtMsg = (TextMessage) message;
+				String msg = txtMsg.getText();
+				System.out.println("Received:" + msg);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String args[]) {
+		Receive rm = new Receive();
+		try {
+			rm.receiveMessage();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- MQ_TEST/src/Send.java
+++ MQ_TEST/src/Send.java
@@ -1,76 +1,84 @@
-import java.util.Random;   
-import javax.jms.Connection;  
-import javax.jms.ConnectionFactory;  
-import javax.jms.DeliveryMode;  
-import javax.jms.Destination;  
-import javax.jms.JMSException;  
-import javax.jms.MessageProducer;  
-import javax.jms.Session;  
-import javax.jms.TextMessage;  
-   
-import org.apache.activemq.ActiveMQConnectionFactory;  
-   
-public class Send {  
-    
-	private static final String url = "failover:(tcp://192.168.13.253:61616,tcp://192.168.11.68:61616)?jms.useAsyncSend=true&randomize=false";
-    private static final String QUEUE_NAME = "G2Queue";  
-   
-    public void sendMessage() throws JMSException {  
-      
-       Connection connection = null;
-       Session session = null;
-       int liveSeconds = 3;
-       try {  
-          
-           ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);  
-           connection = connectionFactory.createConnection();  
-          
-           connection.start();  
-          
-           session = connection.createSession(false,  
-                  Session.AUTO_ACKNOWLEDGE);  
-           
-           Destination destination = session.createQueue(QUEUE_NAME);  
-           
-           MessageProducer producer =session.createProducer(destination);  
-         
-           producer.setDeliveryMode(DeliveryMode.PERSISTENT);  
-            
-           String msg = "";  
-           int i = 0;  
-        do {  
-            msg = "The "+i +"th " + "msg_"+new Random();  
-                TextMessage message = session.createTextMessage(msg);  
-                Thread.sleep(1);  
-                producer.send(message);  
-                if(liveSeconds>0) {
-                	//jmsMessagingTamplate.getJmsTemplate().SetExplicitQosEnabled(true);
-                	//jmsMessagingTamplate.getJmsTemplate().setTimeToLive(liveSeconds*1000);
-                	//producer.SetExplicitQosEnabled(true);
-                	producer.setTimeToLive(liveSeconds*1000);
-                	
-                }
-                
-                System.out.println("The send msg is:" +msg);  
-                i++;  
-        } while (true);  
-       } catch (Exception e) {  
-           e.printStackTrace();  
-       }  finally 
-       {
-    	   connection.close();
-    	   session.close();
-    	   
-       }
-    }  
-   
-    public static void main(String[] args) {  
-       Send sndMsg = new Send();  
-       try {  
-           sndMsg.sendMessage();  
-       } catch (Exception ex) {  
-           System.out.println(ex.toString());  
-       }  
-    }  
-}  
+import java.util.Random;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Send {
+
+	 private static final String url = "failover:(tcp://192.168.13.253:61616,tcp://192.168.11.68:61616)?jms.useAsyncSend=true&randomize=false";
+	//private static final String url = "failover:(ws://192.168.13.253:61614/stomp,ws://192.168.11.68:61614/stomp";
+	private static final String QUEUE_NAME = "G1Queue";
+
+	public void sendMessage() throws JMSException {
+
+		Connection connection = null;
+		Session session = null;
+		int liveSeconds = 3;
+		try {
+
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+			connection = connectionFactory.createConnection();
+
+			connection.start();
+
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			Destination destination = session.createQueue(QUEUE_NAME);
+
+			MessageProducer producer = session.createProducer(destination);
+
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+			String msg = "";
+			int i = 0;
+			do {
+				msg = "The " + QUEUE_NAME + i + "th " + "msg_" + new Random();
+				TextMessage message = session.createTextMessage(msg);
+				Thread.sleep(1);
+				producer.send(message);
+				if (liveSeconds > 0) {
+					// jmsMessagingTamplate.getJmsTemplate().SetExplicitQosEnabled(true);
+					// jmsMessagingTamplate.getJmsTemplate().setTimeToLive(liveSeconds*1000);
+					// producer.SetExplicitQosEnabled(true);
+					producer.setTimeToLive(liveSeconds * 1000);
+				}
+
+				System.out.println("The send msg is:" + msg);
+				i++;
+			} while (true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+					System.out.println("Connection was closed successfully~");
+				}
+
+				if (session != null) {
+					session.close();
+					System.out.println("Session was closed successfully~");
+				}
+
+			} catch (JMSException e) {
+				e.fillInStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Send sndMsg = new Send();
+		try {
+			sndMsg.sendMessage();
+		} catch (Exception ex) {
+			System.out.println(ex.toString());
+		}
+	}
+}
