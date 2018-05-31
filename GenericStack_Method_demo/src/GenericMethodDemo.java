58422b297a14dc7bd44b5173e32de6e2e2e890aa
bkhk06 <bkhk06@sina.com>
2018-05-31 14:24:22 +0800

    update

---
+++ GenericStack_Method_demo/src/GenericMethodDemo.java
@@ -0,0 +1,16 @@
+
+public class GenericMethodDemo {
+
+	public static void main(String[] args) {
+		Integer[] integers= {1,2,3,4,5};
+		String[] strings= {"London","Paris","Beijing","Shanghai","Chengdu"};
+		 
+		GenericMethodDemo.<Integer>print(integers);
+		GenericMethodDemo.<String>print(strings);
+	}
+	
+	public static <E> void print(E[] list) {
+		for (int i=0;i<list.length;i++)
+			System.out.println(list[i]+" ");
+	}
+}
