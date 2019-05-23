4ec82614c4bc457e27f64e8a9e9caa25aca4ce03
bkhk06 <bkhk06@sina.com>
2019-05-23 18:56:06 +0800

    update

--- SpringbootWarApplication/src/main/java/adcc/com/springboot_war/ControllerClass.java
+++ SpringbootWarApplication/src/main/java/adcc/com/springboot_war/ControllerClass.java
@@ -3,10 +3,14 @@ package adcc.com.springboot_war;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+
 @RestController
 public class ControllerClass {
     @RequestMapping("/app")
     public String app(){
-        return "Application started";
+        SimpleDateFormat time=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String TimeString = time.format(new java.util.Date());
+        return ("Application started at： "+TimeString);
     }
 }
--- SpringbootWarApplication/src/main/java/adcc/com/springboot_war/SpringbootWarApplication.java
+++ SpringbootWarApplication/src/main/java/adcc/com/springboot_war/SpringbootWarApplication.java
@@ -2,12 +2,18 @@ package adcc.com.springboot_war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class SpringbootWarApplication {
+public class SpringbootWarApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ControllerClass.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWarApplication.class, args);
     }
