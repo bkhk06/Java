f0ae80202c6cf3882ee44c7902d2ab30d2f4ff09
bkhk06 <bkhk06@sina.com>
2019-05-23 21:16:11 +0800

    update

--- SpringbootWarApplication/src/main/java/adcc/com/springboot_war/SpringbootWarApplication.java
+++ SpringbootWarApplication/src/main/java/adcc/com/springboot_war/SpringbootWarApplication.java
@@ -3,11 +3,11 @@ package adcc.com.springboot_war;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@ServletComponentScan
+@EnableCaching
 public class SpringbootWarApplication extends SpringBootServletInitializer {
 
     @Override
