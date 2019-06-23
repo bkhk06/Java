e9ce011ed70cf111152edd588d4281157c6bd1c7
bkhk06 <bkhk06@sina.com>
2019-06-23 15:16:06 +0800

    update

---
+++ rest-demo3/src/main/java/ld/restdemo3/RestDemo3Application.java
@@ -0,0 +1,13 @@
+package ld.restdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemo3Application.class, args);
+    }
+
+}
---
+++ rest_demo2/src/main/java/ld/rest_demo2/RestDemo2Application.java
@@ -0,0 +1,13 @@
+package ld.rest_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemo2Application.class, args);
+    }
+
+}
