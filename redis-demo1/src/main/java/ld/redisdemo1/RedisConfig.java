693f772f64c057b93c1334255a29f17b9a628db0
bkhk06 <bkhk06@sina.com>
2019-06-26 17:48:00 +0800

    go

---
+++ redis-demo1/src/main/java/ld/redisdemo1/RedisConfig.java
@@ -0,0 +1,42 @@
+package ld.redisdemo1;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * Created by Liu.DA on 2019/6/26
+ */
+@Configuration
+@EnableAutoConfiguration
+public class RedisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis.pool")
+    public JedisConnectionFactory getRedisConfig(){
+       return new JedisConnectionFactory();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory getConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setUsePool(true);
+        JedisPoolConfig config = getRedisConfig();
+        factory.setPoolConfig(config);
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> getRedisTemplate() {
+        JedisConnectionFactory factory = getConnectionFactory();
+        RedisTemplate<?, ?> template = new StringRedisTemplate(factory);
+        return template;
+    }
+
+}
