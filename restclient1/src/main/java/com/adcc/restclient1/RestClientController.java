a693931f2421e9e95caa035c07dd43ca697060ab
bkhk06 <bkhk06@sina.com>
2019-09-29 15:25:32 +0800

    up

---
+++ restclient1/src/main/java/com/adcc/restclient1/RestClientController.java
@@ -0,0 +1,13 @@
+package com.adcc.restclient1;
+
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * Created by Liu.DA on 2019/9/29
+ */
+public class RestClientController {
+    @Value("$rest-api")
+    private static String REST_API;
+
+    
+}
