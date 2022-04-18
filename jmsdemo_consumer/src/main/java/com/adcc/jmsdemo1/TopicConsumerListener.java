bed6f277a0ea9b0ce6b79488beef708da25ec26b
bkhk06 <bkhk06@sina.com>
2022-04-18 16:20:35 +0800

    jms

--- jmsdemo_consumer/src/main/java/com/adcc/jmsdemo1/TopicConsumerListener.java
+++ jmsdemo_consumer/src/main/java/com/adcc/jmsdemo1/TopicConsumerListener.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 /**
  * Created by Liu.DA on 2019/9/19
  */
@@ -20,17 +22,19 @@ public class TopicConsumerListener {
 
     @Value("${topicName}")
     private static String topicName4;
+
+    Date date=new Date();// 获取当前日期
     
     @JmsListener(destination ="${topicName1}",containerFactory = "topicListenerContainerFactory")
     /*,containerFactory = "topicListenerContainerFactory"*/
     public void receieve1(String msg){
-        System.out.println("Topic.Subscribe.receive1接收消息："+msg);
+        System.out.println(new Date()+"Topic.Subscribe.receive1接收消息："+msg);
     }
 
     @JmsListener(destination ="${topicName2}",containerFactory = "topicListenerContainerFactory")
     /*,containerFactory = "topicListenerContainerFactory"*/
     public void receieve2(String msg){
-        System.out.println("Topic.Subscribe.receive2接收消息："+msg);
+        System.out.println(new Date()+"Topic.Subscribe.receive2接收消息："+msg);
     }
 
     //@JmsListener(destination = topicName/*,containerFactory = "topicListenerContainerFactory"*/)
@@ -38,13 +42,13 @@ public class TopicConsumerListener {
     @JmsListener(destination ="${topicName3}",containerFactory = "topicListenerContainerFactory")
     /*,containerFactory = "topicListenerContainerFactory"*/
     public void receieve3(String msg){
-        System.out.println("Topic.Subscribe.receive3接收消息："+msg);
+        System.out.println(new Date()+"Topic.Subscribe.receive3接收消息："+msg);
     }
 
     //@JmsListener(destination = topicName/*,containerFactory = "topicListenerContainerFactory"*/)
     /*,containerFactory = "topicListenerContainerFactory"*/
     @JmsListener(destination ="${topicName4}",containerFactory = "topicListenerContainerFactory")
     public void receieve4(String msg){
-        System.out.println("Topic.Subscribe.receive4接收消息："+msg);
+        System.out.println(new Date()+"Topic.Subscribe.receive4接收消息："+msg);
     }
 }
