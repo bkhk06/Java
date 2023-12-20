c4680fcfe008a38733e528fd5a2af83ffe585f07
bkhk06 <bkhk06@sina.com>
2023-12-20 09:26:13 +0800

    1
    1

--- CdmStarted/src/com/cdm/started/OriginalRadarToObjectServer.java
+++ CdmStarted/src/com/cdm/started/OriginalRadarToObjectServer.java
@@ -1,11 +1,10 @@
 package com.cdm.started;
 
-import org.tanukisoftware.wrapper.WrapperSimpleApp;
 import org.tanukisoftware.wrapper.WrapperStartStopApp;
 
-public class  SectorStatusServer  {
+public class  OriginalRadarToObjectServer  {
 
     public static void main(String[] args) {
-        WrapperSimpleApp.main(args);
+        WrapperStartStopApp.main(args);
     }
 }
--- CdmStarted/src/com/cdm/started/SectorStatusZUUU.java
+++ CdmStarted/src/com/cdm/started/SectorStatusZUUU.java
@@ -3,7 +3,7 @@ package com.cdm.started;
 import org.tanukisoftware.wrapper.WrapperSimpleApp;
 import org.tanukisoftware.wrapper.WrapperStartStopApp;
 
-public class  SectorStatusServer  {
+public class  SectorStatusZUUU  {
 
     public static void main(String[] args) {
         WrapperSimpleApp.main(args);
